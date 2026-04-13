071cd49633caba2e290cfb9436bf55fb5493d2ec
Basavaraj N <95026914+Basavaraj852@users.noreply.github.com>
2026-04-13 22:32:36 +0530

    Add Navbar component for E-Commerce Store

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav style={{background: "#333", color: "#fff", padding: "10px"}}>
+      <h1>E-Commerce Store</h1>
+    </nav>
+  );
+}
